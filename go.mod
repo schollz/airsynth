a6b0665a1eb745556b765908e049041955dc98f6
Zack Scholl <zack.scholl@gmail.com>
2021-06-24 12:07:43 -0700

    works

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/schollz/oschands
 
 go 1.16
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/hypebeast/go-osc v0.0.0-20210408213458-3287e1838f40
+	github.com/schollz/logger v1.2.0
+	golang.org/x/exp v0.0.0-20210624180137-7a70d7cf143a // indirect
+	golang.org/x/tools v0.1.4 // indirect
+	gonum.org/v1/gonum v0.9.2 // indirect
+)
--- main.go
+++ main.go
@@ -4,26 +4,74 @@ import (
 	"embed"
 	"fmt"
 	"io/fs"
+	"io/ioutil"
 	"net/http"
 	"strings"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/hypebeast/go-osc/osc"
+	log "github.com/schollz/logger"
+	"gonum.org/v1/gonum/stat"
 )
+
+var client *osc.Client
 
 //go:embed static
 var static embed.FS
+var fsStatic http.Handler
 
 func main() {
 	fsRoot, _ := fs.Sub(static, "static")
-	fsStatic := http.FileServer(http.FS(fsRoot))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ruri := r.RequestURI
-		fmt.Println(ruri, strings.HasSuffix(ruri, ".js"))
-		if strings.HasSuffix(ruri, ".js") {
+	fsStatic = http.FileServer(http.FS(fsRoot))
+	// client = osc.NewClient("localhost", 57120)
+	log.SetLevel("debug")
+	port := 8098
+	log.Infof("listening on :%d", port)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().UTC()
+	err := handle(w, r)
+	if err != nil {
+		log.Error(err)
+	}
+	log.Infof("%v %v %v %s\n", r.RemoteAddr, r.Method, r.URL.Path, time.Since(t))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) (err error) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	// very special paths
+	if r.URL.Path == "/ws" {
+		return handleWebsocket(w, r)
+	} else {
+		if strings.HasSuffix(r.URL.Path, ".js") {
 			w.Header().Set("Content-Type", "text/javascript")
 		}
 		fsStatic.ServeHTTP(w, r)
-	})
-	fmt.Println("running on port 8080")
-	http.ListenAndServe(":8080", nil)
+		return
+		var b []byte
+		if r.URL.Path == "/" {
+			log.Debug("loading index")
+			b, err = ioutil.ReadFile("static/index.html")
+			if err != nil {
+				return
+			}
+		} else {
+			b, err = ioutil.ReadFile("static" + r.URL.Path)
+			if err != nil {
+				return
+			}
+		}
+		w.Write(b)
+	}
+
+	return
 }
 
 type HandData struct {
@@ -37,4 +85,52 @@ type HandData struct {
 		Score float64 `json:"score"`
 		Label string  `json:"label"`
 	} `json:"multiHandedness"`
+}
+
+var wsupgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleWebsocket(w http.ResponseWriter, r *http.Request) (err error) {
+	c, errUpgrade := wsupgrader.Upgrade(w, r, nil)
+	if errUpgrade != nil {
+		return errUpgrade
+	}
+	defer c.Close()
+
+	for {
+		var p HandData
+		err := c.ReadJSON(&p)
+		if err != nil {
+			log.Debug("read:", err)
+		} else {
+			go processScore(p)
+		}
+	}
+	return
+}
+
+func processScore(p HandData) {
+	for i, hand := range p.MultiHandLandmarks {
+		xs := make([]float64, len(hand))
+		ys := make([]float64, len(hand))
+		zs := make([]float64, len(hand))
+		ws := make([]float64, len(hand))
+		for j, coord := range hand {
+			xs[j] = coord.X
+			ys[j] = coord.Y
+			zs[j] = coord.Z
+			ws[j] = 1
+		}
+		meanX, stdX := stat.MeanStdDev(xs, ws)
+		meanY, stdY := stat.MeanStdDev(xs, ws)
+		meanZ, stdZ := stat.MeanStdDev(xs, ws)
+		_ = meanZ
+		_ = stdZ
+		fmt.Println(p.MultiHandedness[i].Label, meanX, meanY, (stdX+stdY)/2)
+	}
 }
